ed433cbf181ee492b90465429964a8253e6975a2
unknown <cogjava4456@iiht.tech>
2022-12-02 18:05:59 +0530

    Added Update Api in Book service

--- book_service/src/main/java/com/digitalbooks/book/controller/BookServiceController.java
+++ book_service/src/main/java/com/digitalbooks/book/controller/BookServiceController.java
@@ -1,16 +1,18 @@
 package com.digitalbooks.book.controller;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Blob;
 import java.sql.SQLException;
 
 import javax.sql.rowset.serial.SerialException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,17 +35,15 @@ public class BookServiceController {
 	@Autowired
 	BookService bookService;
 	
-//	@GetMapping("/test")
-//	public String test() {
-//		System.out.println("Calling Book service");
-//		String response= restTemplate.getForObject("http://USER-SERVICE/user/test", String.class);
-//		return "Book Service and "+response+" from Book Service";
-//	}
+	@GetMapping("/test")
+	public String test() {
+		System.out.println("Calling Book service");
+		//String response= restTemplate.getForObject("http://USER-SERVICE/user/test", String.class);
+		return "Book Service and  from Book Service";
+	}
 	
 	@PostMapping("/author/{author-id}/books")
     public Books saveBook(@RequestParam(value="file", required=false) MultipartFile file, @ModelAttribute Books book, @PathVariable("author-id") int authorId) {
-
-
 		    byte[] bytes=null;
 		    Blob blob= null;
 			try {
@@ -72,10 +72,11 @@ public class BookServiceController {
 		return book;
     	
     }
-	@PostMapping("/author/{author-id}/books/{book-id}")
-    public void updateBook(@PathVariable("author-id") int authorId , @PathVariable("book-id") int bookId,
-    		@RequestBody Books book) {
-		bookService.updateBook(authorId,bookId,book );
+	@PutMapping("/author/{author-id}/books/{book-id}")
+    public ResponseEntity<?> updateBook(@RequestParam(value="file", required=false) MultipartFile file, @PathVariable("author-id") int authorId , @PathVariable("book-id") int bookId,
+    		@ModelAttribute Books book) {
+		ResponseEntity<?> response= bookService.updateBook(file, authorId,bookId,book );
+		return response;
 	}
 
 
--- book_service/src/main/java/com/digitalbooks/book/service/BookService.java
+++ book_service/src/main/java/com/digitalbooks/book/service/BookService.java
@@ -1,10 +1,20 @@
 package com.digitalbooks.book.service;
 
+import java.io.IOException;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.util.Optional;
+
+import javax.sql.rowset.serial.SerialException;
+
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.digitalbooks.book.model.Books;
+import com.digitalbooks.book.payload.response.MessageResponse;
 import com.digitalbooks.book.repository.BookRepository;
 
 @Service
@@ -19,8 +29,44 @@ public class BookService {
 		
 	}
 
-	public void updateBook(int authorId, int bookId, Books book) {
-		// TODO Auto-generated method stub
+	public ResponseEntity<?> updateBook(MultipartFile file, int authorId, int bookId, Books book) {
+		Optional<Books> existingBook=  bookRepository.findById(bookId);
+		
+		if(!existingBook.isEmpty()) {
+			byte[] bytes=null;
+		    Blob blob= null;
+			try {
+				
+			if(file!=null) {
+				bytes = file.getBytes();
+				blob = new javax.sql.rowset.serial.SerialBlob(bytes);
+			}
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (SerialException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		   
+			book.setLogo(blob);
+			existingBook.get().setLogo(book.getLogo());
+			existingBook.get().setTitle(book.getTitle());
+			existingBook.get().setPrice(book.getPrice());
+			existingBook.get().setPublisher(book.getPublisher());
+			existingBook.get().setActive(book.isActive());
+			existingBook.get().setContent(book.getContent());
+			existingBook.get().setLogo(book.getLogo());
+			existingBook.get().setCategory(book.getCategory());
+			bookRepository.save(existingBook.get());
+			return ResponseEntity.ok().body("Book is updated successfully");
+	
+		}
+		else {
+			return ResponseEntity.badRequest().body(new MessageResponse("Book does not exist!"));
+            
+		}
 		
 	}
 
